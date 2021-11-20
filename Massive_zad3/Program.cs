62afa430c8e27433c318ea5a04d20a80b39895be
nvrsk <nvrsk@DESKTOP-1RBTMCR>
2021-11-20 20:51:12 +0300

    Добавьте файлы проекта.

---
+++ Massive_zad3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Massive_zad3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = 0;
+            Console.WriteLine("Введите кол-во элементов последовательности:");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите последовательность через пробел:");
+            var D = Array.ConvertAll(Console.ReadLine().Split(' '), s => int.Parse(s));
+            for (int i = 0; i < n; i++)
+            {
+                if (i%2!=0)
+                {
+                    count += D[i];
+                }
+              
+            }
+            Console.WriteLine($"Массив D - {string.Join (',', D)}. Сумма его элементов с нечетными индексами - {count}");
+        }
+    }
+}
